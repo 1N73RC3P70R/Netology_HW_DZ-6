5ec514d1f9d4a755b188ab84d97dbbba1b698dc9
1N73RC3P70R <kras.konstantinov@yandex.ru>
2024-07-18 18:41:22 +0300

    Added count

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -1,20 +1,67 @@
 package ru.netology
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.netology.databinding.ActivityMainBinding
+import android.util.Log
+import ru.netology.databinding.MainPageLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainPageLayoutBinding
+    private var likeCount = 999
+    private var shareCount = 1099
+    private var viewCount = 500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = MainPageLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateCounts()
+
+        binding.root.setOnClickListener {
+            Log.d("Тест", "Нажатие!")
+        }
+
+
+        binding.likeImageView.setOnClickListener {
+            Log.d("Тест", "Понравилась запись!")
+            if (binding.likeImageView.tag as? Boolean == true) {
+                likeCount--
+                binding.likeImageView.setImageResource(R.drawable.baseline_favorite_border_24)
+                binding.likeImageView.tag = false
+            } else {
+                likeCount++
+                binding.likeImageView.setImageResource(R.drawable.baseline_favorite_24_filled)
+                binding.likeImageView.tag = true
+            }
+            updateCounts()
+        }
+
+        binding.shareImageView.setOnClickListener {
+            Log.d("Тест", "Поделился!")
+            if (binding.shareImageView.tag as? Boolean == true){
+                shareCount--
+                binding.shareImageView.tag = false
+            } else {
+                shareCount++
+                binding.shareImageView.tag = true
+            }
+
+            updateCounts()
+        }
+    }
+    private fun updateCounts(){
+        binding.likeCountTextView.text = formatCount(likeCount)
+        binding.shareCountTextView.text = formatCount(shareCount)
+        binding.viewCountTextView.text = formatCount(viewCount)
+    }
+    private fun formatCount(count: Int): String {
+        return when {
+            count < 1000 -> count.toString()
+            count < 10000 -> "${count / 1000}.${(count % 1000) / 100}k"
+            count < 1000000 -> "${count / 1000}k"
+            else -> String.format("%.1fM", count / 1000000.0)
         }
     }
 }
